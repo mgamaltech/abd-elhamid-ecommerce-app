42faf5781b8bd409794a9d8131b823c551d5948f
aabdehamid <aabdehamidkh@gmail.com>
2026-09-12 13:11:47 +0300

    adding products code (I didn't know it wasn't added

---
+++ products.php
@@ -0,0 +1,32 @@
+<?php
+
+$products = [
+    ["name" => "kofta" ,"price" => 500, "in_stock" => true],
+    ["name" => "salata" , "price" => 200, "in_stock" => false],
+    ["name" => "kago" , "price" => 900, "in_stock" => true],
+    ["name" => "mango" , "price" => 300, "in_stock" => false],
+    ["name" => "apple" , "price" => 100, "in_stock" => false],
+];
+
+$available_in_stock = [];
+foreach ($products as $product => $value) {
+
+    if($value["in_stock"] == true)
+    {
+        $available_in_stock[] = $value;
+
+    }
+
+}
+
+// var_dump($available_in_stock);
+
+uasort($products, function ($a, $b) {
+    return $a["price"] <=> $b["price"];
+});
+echo "The available products in stock are: ";
+foreach ($products as $product) {
+    echo $product["name"] . " - " . $product["price"] . "\n";
+}
+
+// var_dump($products);
